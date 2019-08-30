bfc5db7f6ca8f18fe95d37745865c1eb351b7552
Eren Okka <erenokka@gmail.com>
2019-08-30 15:55:06 +0300

    Improve header callback

--- include/hypr/detail/curl_callback.hpp
+++ include/hypr/detail/curl_callback.hpp
@@ -3,6 +3,8 @@
 #include <string_view>
 
 #include <curl/curl.h>
+#include <hypp/detail/syntax.hpp>
+#include <hypp/parser/header.hpp>
 #include <hypp/parser/response.hpp>
 
 #include <hypr/models.hpp>
@@ -19,8 +21,8 @@ int debug_callback(CURL* handle, curl_infotype type, char* data, size_t size,
     case CURLINFO_HEADER_OUT:
     case CURLINFO_DATA_IN:
     case CURLINFO_DATA_OUT:
-    case CURLINFO_SSL_DATA_OUT:
     case CURLINFO_SSL_DATA_IN:
+    case CURLINFO_SSL_DATA_OUT:
       // @TODO
       break;
   }
@@ -30,27 +32,39 @@ int debug_callback(CURL* handle, curl_infotype type, char* data, size_t size,
 
 size_t header_callback(char* buffer, size_t size, size_t nitems,
                        void* userdata) {
-  const std::string_view data{buffer, size * nitems};
+  const std::string_view line{buffer, size * nitems};
 
-  if (userdata && !data.empty()) {
-    auto& response = *static_cast<Response*>(userdata);
-    if (data._Starts_with("HTTP/")) {
-      hypp::Parser parser{data};
-      const auto expected = hypp::ParseStatusLine(parser);
+  if (userdata && !line.empty()) {
+    auto& response = *static_cast<hypr::Response*>(userdata);
+
+    const auto parse_status_line = [&line]() {
+      hypp::Parser parser{line};
+      return hypp::ParseStatusLine(parser);
+    };
+    const auto parse_header_field = [&line]() {
+      hypp::Parser parser{line};
+      return hypp::ParseHeaderField(parser);
+    };
+
+    if (const auto expected = parse_status_line()) {
       response.start_line = std::move(expected.value());
-    } else if (data != "\r\n") {
-      hypp::Parser parser{data};
-      const auto expected = hypp::ParseHeaderField(parser);
-      response.header.fields.push_back(std::move(expected.value()));
+      response.header.fields.clear();
+
+    } else if (const auto expected = parse_header_field()) {
+      response.header.fields.emplace_back(std::move(expected.value()));
+
+    } else if (line != hypp::detail::syntax::kCRLF) {
+      // @TODO: Handle error
     }
   }
 
-  return data.size();
+  return line.size();
 }
 
 int progress_callback(void* clientp,
                       curl_off_t dltotal, curl_off_t dlnow,
                       curl_off_t ultotal, curl_off_t ulnow) {
+  // @TODO
   return 0;
 }
 
@@ -58,7 +72,7 @@ size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata) {
   const std::string_view data{ptr, size * nmemb};
 
   if (userdata && !data.empty()) {
-    auto& response = *static_cast<Response*>(userdata);
+    auto& response = *static_cast<hypr::Response*>(userdata);
     response.body.append(data);
   }
 
